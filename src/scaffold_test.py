b365480e2a5cf0d16b57a93dc6913ca0d283d3af
David Barrat <david@barrat.io>
2021-01-14 07:53:20 +0100

    fix linter

--- src/scaffold_test.py
+++ src/scaffold_test.py
@@ -1,13 +1,9 @@
-# from unittest.mock import MagicMock
-from pytest_mock import mocker
 from .scaffold import getPrompt
 
-def test_buildStrategies():
-    pass
 
 def test_getPrompt(mocker):
-    mocker.patch('builtins.input', return_value="toto")
-    assert getPrompt('dummy prompt text') == "toto"
+    mocker.patch("builtins.input", return_value="toto")
+    assert getPrompt("dummy prompt text") == "toto"
 
-    mocker.patch('builtins.input', return_value="9b4db9b9-2b16-4347-ab55-38c47bbffaee")
-    assert getPrompt('dummy prompt text') == "9b4db9b9-2b16-4347-ab55-38c47bbffaee"
+    mocker.patch("builtins.input", return_value="9b4db9b9-2b16-4347")
+    assert getPrompt("dummy prompt text") == "9b4db9b9-2b16-4347"
