0346da584021a4f328ff76e175f244d29121565a
David Barrat <david@barrat.io>
2021-01-18 07:12:51 +0100

    adding openshift client python to the dependencies

--- ocpeasy/deploy.py
+++ ocpeasy/deploy.py
@@ -1,6 +1,7 @@
-def deploy(strategy=3.4):
+def deploy(stageId: str):
     # TODO: check if the project has been initialized / ocpeasy.yml exists
     # TODO: check if the selected stage exists in the ocpeasy.yml file
     # (otherwise ask user if it should be created)
+    # TODO: get oc path from environment variable, if not simply call `oc`
 
-    return f"Deploy {strategy}"
+    return f"Deploy {stageId}"
