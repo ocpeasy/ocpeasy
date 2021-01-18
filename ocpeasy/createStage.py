c4f37e38aa42d527ad2fca0af46e5a091dd48e74
David Barrat <david@barrat.io>
2021-01-18 01:50:37 +0100

    cli target createStage can generate the configuration files

--- ocpeasy/createStage.py
+++ ocpeasy/createStage.py
@@ -5,16 +5,15 @@ from .utils import (
     cloneStrategyRepository,
     cleanWorkspace,
     getPrompt,
+    replaceAll,
 )
-from .constants import (
-    OCPEASY_CONFIG_NAME,
-    OCPEASY_CONTEXT_PATH,
-)
+from .constants import OCPEASY_CONFIG_NAME, OCPEASY_CONTEXT_PATH, CLI_NAME
 import yaml
-import shutil
+
+from .__version__ import __version__
 
 
-def createStage(stageId: str):
+def createStage():
     projectEnvPath = getenv("POETRY_DEV_PATH", None)
     pathProject = "." if not projectEnvPath else removeTrailSlash(projectEnvPath)
 
@@ -38,30 +37,22 @@ def createStage(stageId: str):
             for excluded in excludedKeys:
                 del globalValues[excluded]
 
-            print(globalValues.keys())
             cloneStrategyRepository(sessionId)
-
-            # ocpTemplateFiles = ["bc", "dc", "img", "route", "svc"]
-            # get configuration corresponding to the base app
-            # f"/tmp/{sessionId}/{strategy}/profiles/{deployConfigDict['profile']}"
-            print(deployConfigDict)
-
             OCPEASY_DEPLOYMENT_PATH = f"{pathProject}/{OCPEASY_CONTEXT_PATH}"
             try:
-                shutil.rmtree(OCPEASY_DEPLOYMENT_PATH, ignore_errors=True)
-                mkdir(OCPEASY_DEPLOYMENT_PATH)
+                # shutil.rmtree(OCPEASY_DEPLOYMENT_PATH, ignore_errors=True)
+                if not path.exists(OCPEASY_DEPLOYMENT_PATH):
+                    mkdir(OCPEASY_DEPLOYMENT_PATH)
 
                 stageConfiguration = tokenConfiguration = {}
 
-                # TODO: check stageId doesnt exist
-                # get stageId
-                # get openshift project name (ocpProject)
-                # get openshift container id (containerId)
-                # configure openshift route (openshiftRoute)
+                # get from CLI (e.g.: --stage=dev) ?
+                # TODO: check if stage already exists
                 stageId = getPrompt(
                     f"What's the id of your stage (default: development)", "development"
                 )
                 ocpProject = getPrompt(f"What's the name of the OpenShift project")
+                # TODO: check if the container name already exists for the project
                 containerId = getPrompt(
                     f"What's the OpenShift container ID/Name (unique per project)"
                 )
@@ -82,31 +73,36 @@ def createStage(stageId: str):
                 tokenConfiguration["containerId"] = containerId
                 tokenConfiguration["containerRouter"] = containerRouter
                 tokenConfiguration["podReplicas"] = podReplicas
+                tokenConfiguration["generatedBy"] = f"{CLI_NAME} CLI ({__version__})"
 
                 # TODO: get from config deployment
                 tokenConfiguration["gitRepository"] = ""
                 tokenConfiguration["gitCredentialsId"] = ""
 
-                print(stageConfiguration)
-                # TODO: append stage configuration to ocpeasy.yml
-
-                # loop into config from
-                print(deployConfigDict.get("templateMeta"))
-
                 strategyId = deployConfigDict["templateMeta"]["strategy"]
-
                 OCP_PROFILE_PATH = f"/tmp/{sessionId}/{strategyId}/profiles/{deployConfigDict['templateMeta']['profile']}"
                 _, _, configFiles = next(walk(OCP_PROFILE_PATH))
-                # configFiles = ['bc.yaml', 'svc.yaml', 'dc.yaml', 'route.yaml', 'img.yaml']
+                # sample: configFiles = ['bc.yaml', 'svc.yaml', 'dc.yaml', 'route.yaml', 'img.yaml']
+                STAGE_CONFIG_ROOT = f"{OCPEASY_DEPLOYMENT_PATH}/{stageId}"
+                if not path.exists(STAGE_CONFIG_ROOT):
+                    mkdir(STAGE_CONFIG_ROOT)
+
                 for configFile in configFiles:
                     configurationPath = f"{OCP_PROFILE_PATH}/{configFile}"
                     with open(configurationPath) as f:
                         configAsDict = yaml.load(f, Loader=yaml.FullLoader)
-                        print(configAsDict)
+                        ocpContextYaml = yaml.dump(configAsDict)
+                        ocpContextBuild = replaceAll(ocpContextYaml, tokenConfiguration)
+                        # generate yaml files in .ocpeasy/stage/{configFile}
+                        stageConfigFile = f"{STAGE_CONFIG_ROOT}/{configFile}"
+                        with open(stageConfigFile, "w") as configTarget:
+                            configTarget.write(ocpContextBuild)
 
             except OSError:
                 print("Creation of the directory %s failed" % OCPEASY_CONTEXT_PATH)
 
             cleanWorkspace(sessionId)
 
-    print(f"buildStage {stageId} {pathProject}")
+    print(
+        f"\n\n -> new OpenShift stage created ({stageId}) for project [{pathProject}] \u2713"
+    )
