1adf142684d30ac17d30659f88767ff069393052
David Barrat <david@barrat.io>
2021-02-17 02:47:14 +0100

    list kind modules

--- ocpeasy/compose.py
+++ ocpeasy/compose.py
@@ -1,13 +1,44 @@
 # from git import Repo
 # from os import walk
-# from simple_term_menu import TerminalMenu
-# import yaml
+from simple_term_menu import TerminalMenu
+import yaml
+
 # import shutil
 
-from .utils import createNewSessionId, cleanWorkspace, prepareWorkspace
+from .utils import (
+    buildMenuOptions,
+    createNewSessionId,
+    cleanWorkspace,
+    prepareWorkspace,
+)
+
+from .constants import (
+    MENU_CURSOR_STYLE,
+    SHOW_SEARCH_HINT,
+)
+
+
+def getModuleTypes(PATH_MODULES: str):
+    with open(PATH_MODULES) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        sortedModules = sorted(list(map(lambda x: x["kind"], data)))
+        modulesList = buildMenuOptions(sortedModules)
+        terminal_menu = TerminalMenu(
+            modulesList,
+            title="Select a module type:",
+            menu_cursor_style=MENU_CURSOR_STYLE,
+            show_search_hint=SHOW_SEARCH_HINT,
+        )
+        menu_entry_index = terminal_menu.show()
+        return sortedModules[menu_entry_index]
 
 
 def compose():
     sessionUuid = createNewSessionId()
     prepareWorkspace(sessionUuid)
+
+    PATH_MODULES = f"/tmp/{sessionUuid}/modules/latest.yml"
+
+    moduleType = getModuleTypes(PATH_MODULES)
+    print(moduleType)
     cleanWorkspace(sessionUuid)
